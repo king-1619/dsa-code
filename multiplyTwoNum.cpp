57ac3eb2013ecb0f75edb8b85cf0d6be48185c89
Omkar Date <omkardate29@gmail.com>
2022-10-12 23:46:49 +0530

    Added cpp code to multiply two numbers

---
+++ multiplyTwoNum.cpp
@@ -0,0 +1,13 @@
+// Write a program in C++ to multipoly 2 numbers
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x, y, ans;
+  cout << "Enter two numbers_" << endl;
+  cin >> x >> y;
+  ans = x * y;
+  cout << "Multiplication of " << x << " and " << y << ": " << ans;
+  return 0;
+}
